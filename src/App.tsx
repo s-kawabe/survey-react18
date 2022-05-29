3451996d0000d0e9106abe79949b49dbfdf7a2eb
s-kawabe <s.kawabe2281@gmail.com>
2022-05-29 11:38:21 +0900

    fix: remove tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import { Link, Outlet, ReactLocation, Router } from "react-location"
-import { routes } from "./router/Route"
-import "./App.css"
+import { Link, Outlet, ReactLocation, Router } from 'react-location'
+import { routes } from './router/Route'
+import './App.css'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+      suspense: true,
+    },
+  },
+})
 
 const App = () => {
   const location = new ReactLocation()
   return (
-    <Router routes={routes} location={location}>
-      <div className="container">
-        <h1>React 18 Survey 🤔</h1>
-        <nav>
-          <ul className="navs">
-            <li className="link"><Link to="/">Top</Link></li>
-            <li className="link"><Link to="/useTransition">useTransition</Link></li>
-            <li className="link"><Link to="/useDeferredValue">useDefferedValue</Link></li>
-            <li className="link"><Link to="/useId">useId</Link></li>
-          </ul>
-        </nav>
-        <Outlet />
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router routes={routes} location={location}>
+        <div className="container">
+          <nav>
+            <ul className="navs">
+              <li className="link">
+                <Link to="/">Top</Link>
+              </li>
+              <li className="link">
+                <Link to="/automaticBatching">Automatic Batching</Link>
+              </li>
+              <li className="link">
+                <Link to="/useTransition">useTransition</Link>
+              </li>
+              <li className="link">
+                <Link to="/useDeferredValue">useDefferedValue</Link>
+              </li>
+              <li className="link">
+                <Link to="/useId">useId</Link>
+              </li>
+            </ul>
+          </nav>
+          <h1>React 18 Survey 🤔</h1>
+          <Outlet />
+        </div>
+      </Router>
+    </QueryClientProvider>
   )
 }
 
