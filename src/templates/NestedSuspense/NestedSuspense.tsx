7f2d3d5436cd3e04381d5a9e258b5cd0b6fe6062
s-kawabe <s.kawabe2281@gmail.com>
2022-07-18 17:48:20 +0900

    feat: nested suspense

--- src/templates/NestedSuspense/NestedSuspense.tsx
+++ src/templates/NestedSuspense/NestedSuspense.tsx
@@ -1,3 +1,37 @@
+import { Suspense } from 'react'
+import { FetchTasks } from '../../components/fetch/FetchTasks'
+import { FetchUsers } from '../../components/fetch/FetchUsers'
+import { Loading } from '../../components/shared/Loading'
+import styles from './NestedSuspense.module.css'
+
 export const NestedSuspense = () => {
-  return <div>NestedSuspense</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>Nested Suspense</div>
+
+      {/* Suspenseにおいて表示されるfallbackは常に一個だけなので、発生したサスペンドから一番近い親要素のfallbackが常に1つ表示されている状態 */}
+      <Suspense
+        fallback={
+          // 2
+          <>
+            <p className={styles.outerText}>Showing outer skeleton</p>
+            <Loading />
+          </>
+        }
+      >
+        <FetchUsers /> {/* 1 */}
+        <Suspense
+          fallback={
+            // 4
+            <>
+              <p className={styles.innerText}>Showing inner skeleton</p>
+              <Loading />
+            </>
+          }
+        >
+          <FetchTasks /> {/* 3 */}
+        </Suspense>
+      </Suspense>
+    </div>
+  )
 }
