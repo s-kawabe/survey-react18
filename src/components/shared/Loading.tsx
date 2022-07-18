7aec45e8ad4eb8740c1bdaedf5c19ac0cae160a5
s-kawabe <s.kawabe2281@gmail.com>
2022-07-18 18:12:42 +0900

    fix: add transition

--- src/components/shared/Loading.tsx
+++ src/components/shared/Loading.tsx
@@ -1,5 +1,5 @@
 import loadingSvg from '../../assets/loading.svg'
 
 export const Loading = () => {
-  return <img src={loadingSvg} alt="ローディング" width={100} height={100} />
+  return <img src={loadingSvg} alt="ローディング" />
 }
--- src/templates/Transition/Transition.tsx
+++ src/templates/Transition/Transition.tsx
@@ -1,3 +1,45 @@
+import { useState, useEffect, useTransition } from 'react'
+import axios from 'axios'
+import styles from './Transition.module.css'
+import { Loading } from '../../components/shared/Loading'
+
 export const Transition = () => {
-  return <div>Transition</div>
+  const [photos, setPhotos] = useState<any>([])
+  const [input, setInput] = useState('') // 見せる用のstate (優先度高)
+  const [searchKey, setSearchKey] = useState('') // API request用のstate (優先度低)
+  const [isPending, startTransition] = useTransition()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios('https://jsonplaceholder.typicode.com/photos')
+      setPhotos(res.data)
+    }
+    fetchData()
+  }, [])
+  const filteredPhotos = photos.filter((photo: any) => {
+    return photo.title.includes(searchKey)
+  })
+  const updateHandler = (e: any) => {
+    // こちらの更新が優先され、文字入力が素早くフィードバックされる
+    setInput(e.target.value)
+    // 緊急性の低いsearchKeyの更新を遅くする
+    startTransition(() => setSearchKey(e.target.value))
+
+    // transitionを使わないと、UIの更新に時間がかかる
+    // setSearchKey(e.target.value)
+  }
+  return (
+    <div className={styles.container}>
+      <p className={styles.title}>startTransition (concurrent feature)</p>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <input type="text" value={input} onChange={updateHandler} />
+        {isPending && <p style={{ fontSize: '18px' }}>pending...</p>}
+      </div>
+      <div className={styles.items}>
+        {filteredPhotos.map((photo: any) => (
+          <p key={photo.id}>{photo.title}</p>
+        ))}
+      </div>
+    </div>
+  )
 }
